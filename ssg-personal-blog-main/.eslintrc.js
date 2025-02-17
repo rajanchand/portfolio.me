2d6e5501dbd05466231ea901b6ab40265b0df209
Rajan Prakash Chand <rajanprakash.chand08@gmail.com>
2025-02-17 19:29:03 +0000

    rajan final

---
+++ ssg-personal-blog-main/.eslintrc.js
@@ -0,0 +1,38 @@
+module.exports = {
+  extends: [
+    'eslint:recommended',
+    'plugin:astro/recommended',
+    'plugin:react/recommended',
+    'plugin:react-hooks/recommended',
+    'plugin:@typescript-eslint/recommended',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  plugins: ['astro', 'react', 'react-hooks', 'import', '@typescript-eslint'],
+  env: {
+    browser: true,
+    node: true,
+    es6: true,
+  },
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+  rules: {
+    'react-hooks/rules-of-hooks': 'error',
+    'react-hooks/exhaustive-deps': 'warn',
+    'react/prop-types': 'off',
+    'import/no-unresolved': 'error',
+    'import/named': 'error',
+    'import/default': 'error',
+    'import/namespace': 'error',
+    '@typescript-eslint/no-explicit-any': 'off',
+  },
+};
